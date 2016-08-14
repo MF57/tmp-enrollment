d4565fd7b762228c9952407db256385a34086b2c
MF57 <p-bochenek@outlook.com>
2016-08-14 19:40:54 +0200

    properties dev + mongo

---
+++ src/main/java/org/tmp/enrollment/config/DevMongoConfig.java
@@ -0,0 +1,27 @@
+package org.tmp.enrollment.config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * Created by mf57 on 14.08.2016.
+ */
+@Configuration
+@EnableMongoRepositories({"org.tmp"})
+@Profile("development")
+public class DevMongoConfig extends AbstractMongoConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "tmp-enrollment";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient("localhost", 27017);
+    }
+}
