f553bbc4bda7a3f762435f2565ae03c42d67af1e
MF57 <p-bochenek@outlook.com>
2016-08-14 18:36:11 +0200

    init

---
+++ src/main/java/org/tmp/enrollment/launcher/Launcher.java
@@ -0,0 +1,22 @@
+package org.tmp.enrollment.launcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by mf57 on 14.08.2016.
+ */
+@SpringBootApplication
+@ComponentScan("org.tmp")
+@Configuration
+@EnableAutoConfiguration
+public class Launcher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
+
+}
